14ab1b943f3fb691c852aae4eb9f098f977762d6
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-15 23:46:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ def index():
 
 @app.route("/", methods=["POST"])
 def process_form():
-    start = flask.request.form.get("start", "")
+    # start = flask.request.form.get("start", "")
 
-    if start:
-        command = "jsub -N mvn11 python310 ./core8/pwb.py mvn/mvnew"
-        result = subprocess.call(command, shell=True, capture_output=True, text=True)
-        return flask.render_template("home.html", result=result)
+    # if start:
+    command = "jsub -N mvn11 python310 ./core8/pwb.py mvn/mvnew"
+    # result = subprocess.call(command, shell=True, capture_output=True, text=True)
+    result = subprocess.call(command)
+    return flask.render_template("home.html", result=result)
