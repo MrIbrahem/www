3fddaf14fe7b30b2d38e184b91bf52f28f551da9
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-15 23:53:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,5 @@ def process_form():
 
     # if start:
     command = "toolforge jobs run mvn11 --image python3.9 --command \\\"python3 core8/pwb.py mvn/mvnew\\\""
-    # result = subprocess.call(command, shell=True, capture_output=True, text=True)
-    result = subprocess.call(command)
-    return flask.render_template("home.html", result=result)
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return flask.render_template("index.html", result=result.stdout)
