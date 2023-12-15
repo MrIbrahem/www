f66112a73c1ea3c4bc3c47c09349093e00e5703f
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-15 23:18:36 +0300

    u

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import flask
+import subprocess
 
 app = flask.Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template("home.html")
+
+
+@app.route("/", methods=["POST"])
+def process_form():
+    start = flask.request.form.get("start", "")
+
+    if start:
+        command = "jsub -N mvn11 python310 ./core8/pwb.py mvn/mvnew"
+        result = subprocess.call(command, shell=True, capture_output=True, text=True)
+        return flask.render_template("index.html", result=result)
