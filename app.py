b3ba87455302f788ed22a5e5067e6149e0cca99d
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-16 00:41:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,18 +15,18 @@ def process_form():
 
     # if start:
     command = '/usr/bin/toolforge jobs run mvn11 --image python3.9 --command "python3 core8/pwb.py mvn/mvnew"'
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, env={"PATH": "/usr/bin"})
     return flask.render_template("home.html", result=result.stdout, co=command)
 
 
 @app.route("/uu")
 def start():
     command = "jobs.sh"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(command, shell=True, capture_output=True, text=True, env={"PATH": "/usr/bin"})
     return flask.render_template("home.html", result=result.stdout, co=command)
 
 @app.route("/jobs")
 def jobs():
     command = '/usr/bin/toolforge jobs list'
-    result = subprocess.call(command, shell=True, capture_output=True, text=True)
+    result = subprocess.call(command, shell=True, capture_output=True, text=True, env={"PATH": "/usr/bin"})
     return flask.render_template("home.html", result=result, co=command)
